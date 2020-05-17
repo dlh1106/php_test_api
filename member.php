f9ba423dbf2235aafd2c531a072481f1fd15ebc8
dohoon <dlh1106@naver.com>
2020-05-17 19:29:38 +0900

    test

---
+++ member.php
@@ -0,0 +1,22 @@
+<?
+class member {
+   
+    function add(string $kname, string $nickname, string $upw, string $mobile, string $email, string $gender="") {
+        "testtest";
+    }
+
+    function view(){
+
+    }
+
+    function list(){
+
+    }
+
+
+}
+
+
+
+
+?>
